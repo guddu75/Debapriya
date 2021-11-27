ede54c932a5c0883501dafc4b2928430aad03bcf
Debapriya Majumder <guddumajumder75@gmail.com>
2021-11-27 21:30:53 +0530

    Clears console

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
-console.log(`Debapriya Majumder
+
+const clearConsole = require('clear-any-console');
+clearConsole();
+console.log(`
+Debapriya Majumder
 
 Undergard Student at Jalpaiguri Government Enginnering College
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+ dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+ officia deserunt mollit anim id est laborum.
+
+
 `);
