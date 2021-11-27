d09e16983267c459cab7d21b511782e50d0d2d1f
Debapriya Majumder <guddumajumder75@gmail.com>
2021-11-27 23:04:45 +0530

    Welcomes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const welcome = require('cli-welcome');
+const pkgJSON = require('./package.json');
+
+welcome({
+	title: pkgJSON.name,
+	tagLine: `Hi. 🙌 Nice to meet you`,
+    description : pkgJSON.description,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+	version: pkgJSON.version
+});
+
 console.log(`
 Debapriya Majumder
 
